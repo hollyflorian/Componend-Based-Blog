eeb967503ddfb7da8234b81dd7e72890b7f5e1c0
florianholly <florian.holly@towa.at>
2020-04-14 00:21:21 +0200

    Change generator links / add Generate All Pages to entries

--- generator/GeneratePage.php
+++ generator/GeneratePage.php
@@ -4,15 +4,16 @@
     $frontendFolder = $_POST["frontendfolder"];
     $innerHtml = $_POST["innerhtml"];
 
+
     // Generate Folder and Open File
-    if(!is_dir("../".$frontendFolder."/".$pagename)){
-        mkdir("../".$frontendFolder."/".$pagename); 
+    if(!is_dir($_SERVER["DOCUMENT_ROOT"]."/".$frontendFolder."/".$pagename)){
+        mkdir($_SERVER["DOCUMENT_ROOT"]."/".$frontendFolder."/".$pagename); 
     }
 
     // Generate new Files and get Content
-    $newFile = fopen("../".$frontendFolder."/".$pagename."/index.php", "w");
+    $newFile = fopen($_SERVER["DOCUMENT_ROOT"]."/".$frontendFolder."/".$pagename."/index.php", "w");
     $content = file_get_contents("PageHead.php");
-
+    
     // Generate File
     fwrite($newFile, "<!-- Generated with HollyCMS 0.0.2 -->");
     fwrite($newFile, $content);
